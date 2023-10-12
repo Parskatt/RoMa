b88d455587cae41f98cefd6deef3a0a84d354be6
qkqhd222 <qkqhd222@kaist.ac.kr>
2023-10-12 16:00:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="roma",
-    packages=["roma"],
+    packages=find_packages(include=("roma*",)),
     version="0.0.1",
     author="Johan Edstedt",
     install_requires=open("requirements.txt", "r").read().split("\n"),
