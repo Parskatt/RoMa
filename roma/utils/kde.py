6a0570ce1449959bffc8dfc2a2ef9cf76dcb5962
Johan Edstedt <johan.edstedt@liu.se>
2024-02-07 15:14:39 +0100

    Update kde.py

--- roma/utils/kde.py
+++ roma/utils/kde.py
@@ -6,3 +6,14 @@ def kde(x, std = 0.1):
     scores = (-torch.cdist(x,x)**2/(2*std**2)).exp()
     density = scores.sum(dim=-1)
     return density
+
+
+def approx_kde(x, std = 0.1, max_num_cmp = 30_000):
+    # use a gaussian kernel to estimate density
+    if len(x.shape) > 2:
+        raise ValueError(f"Needs shape N, D got shape {x.shape}")
+    x = x.half() # Do it in half precision TODO: remove hardcoding
+    y = torch.multinomial(x, min(max_num_cmp,len(x.shape[-2])), replacement=False)
+    scores = (-torch.cdist(x,y)**2/(2*std**2)).exp()
+    density = scores.sum(dim=-1)
+    return density
