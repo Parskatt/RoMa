986f01b47608498f6acfbc613ab615f69d6a7af2
Dmytro Mishkin <ducha.aiki@gmail.com>
2024-02-23 15:38:16 +0100

    Update kde.py

--- roma/utils/kde.py
+++ roma/utils/kde.py
@@ -13,7 +13,7 @@ def approx_kde(x, std = 0.1, max_num_cmp = 20_000):
     if len(x.shape) > 2:
         raise ValueError(f"Needs shape N, D got shape {x.shape}")
     x = x.half() # Do it in half precision TODO: remove hardcoding
-    y = torch.multinomial(x, min(max_num_cmp,len(x.shape[-2])), replacement=False)
+    y = torch.multinomial(x, min(max_num_cmp,x.shape[-2]), replacement=False)
     scores = (-torch.cdist(x,y)**2/(2*std**2)).exp()
     density = scores.sum(dim=-1)
     return density
