691fcd58aa539ad4a5b0006a4f3240a98e770e93
Johan Edstedt <johan.edstedt@liu.se>
2024-05-22 19:58:03 +0200

    more robust loss experiment, destroys all prev api

--- roma/losses/robust_loss_tiny_roma.py
+++ roma/losses/robust_loss_tiny_roma.py
@@ -18,8 +18,7 @@ class RobustLosses(nn.Module):
         scale_normalize=False,
         ce_weight=0.01,
         local_loss=True,
-        local_dist=4.0,
-        local_largest_scale=8,
+        local_dist=None,
         smooth_mask = False,
         depth_interpolation_mode = "bilinear",
         mask_depth_loss = False,
@@ -29,13 +28,14 @@ class RobustLosses(nn.Module):
         epe_mask_prob_th = None,
     ):
         super().__init__()
+        if local_dist is None:
+            local_dist = {}
         self.robust = robust  # measured in pixels
         self.center_coords = center_coords
         self.scale_normalize = scale_normalize
         self.ce_weight = ce_weight
         self.local_loss = local_loss
         self.local_dist = local_dist
-        self.local_largest_scale = local_largest_scale
         self.smooth_mask = smooth_mask
         self.depth_interpolation_mode = depth_interpolation_mode
         self.mask_depth_loss = mask_depth_loss
@@ -86,6 +86,8 @@ class RobustLosses(nn.Module):
 
     def regression_loss(self, x2, prob, flow, certainty, scale, eps=1e-8, mode = "delta"):
         epe = (flow.permute(0,2,3,1) - x2).norm(dim=-1)
+        if scale in self.local_dist:
+            prob = prob * (epe < (2 / 512) * (self.local_dist[scale] * scale)).float()
         if scale == 1:
             pck_05 = (epe[prob > 0.99] < 0.5 * (2/512)).float().mean()
             wandb.log({"train_pck_05": pck_05}, step = roma.GLOBAL_STEP)
@@ -112,7 +114,6 @@ class RobustLosses(nn.Module):
         scales = list(corresps.keys())
         tot_loss = 0.0
         # scale_weights due to differences in scale for regression gradients and classification gradients
-        scale_weights = {1:1, 2:1, 4:1, 8:1, 16:1}
         for scale in scales:
             scale_corresps = corresps[scale]
             scale_certainty, flow_pre_delta, delta_cls, offset_scale, scale_gm_cls, scale_gm_certainty, flow, scale_gm_flow = (
@@ -143,28 +144,23 @@ class RobustLosses(nn.Module):
         )
             x2 = gt_warp.float()
             prob = gt_prob
-            
-            if self.local_largest_scale >= scale:
-                prob = prob * (
-                        F.interpolate(prev_epe[:, None], size=(h, w), mode="nearest-exact")[:, 0]
-                        < (2 / 512) * (self.local_dist[scale] * scale))
-            
+                        
             if scale_gm_cls is not None:
                 gm_cls_losses = self.gm_cls_loss(x2, prob, scale_gm_cls, scale_gm_certainty, scale)
                 gm_loss = self.ce_weight * gm_cls_losses[f"gm_certainty_loss_{scale}"] + gm_cls_losses[f"gm_cls_loss_{scale}"]
-                tot_loss = tot_loss + scale_weights[scale] * gm_loss
+                tot_loss = tot_loss + gm_loss
             elif scale_gm_flow is not None:
                 gm_flow_losses = self.regression_loss(x2, prob, scale_gm_flow, scale_gm_certainty, scale, mode = "gm")
                 gm_loss = self.ce_weight * gm_flow_losses[f"gm_certainty_loss_{scale}"] + gm_flow_losses[f"gm_regression_loss_{scale}"]
-                tot_loss = tot_loss + scale_weights[scale] * gm_loss
+                tot_loss = tot_loss +  gm_loss
             
             if delta_cls is not None:
                 delta_cls_losses = self.delta_cls_loss(x2, prob, flow_pre_delta, delta_cls, scale_certainty, scale, offset_scale)
                 delta_cls_loss = self.ce_weight * delta_cls_losses[f"delta_certainty_loss_{scale}"] + delta_cls_losses[f"delta_cls_loss_{scale}"]
-                tot_loss = tot_loss + scale_weights[scale] * delta_cls_loss
+                tot_loss = tot_loss + delta_cls_loss
             else:
                 delta_regression_losses = self.regression_loss(x2, prob, flow, scale_certainty, scale)
                 reg_loss = self.ce_weight * delta_regression_losses[f"delta_certainty_loss_{scale}"] + delta_regression_losses[f"delta_regression_loss_{scale}"]
-                tot_loss = tot_loss + scale_weights[scale] * reg_loss
+                tot_loss = tot_loss + reg_loss
             prev_epe = (flow.permute(0,2,3,1) - x2).norm(dim=-1).detach()
         return tot_loss
