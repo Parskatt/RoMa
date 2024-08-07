6cb19ad865bf83125938f39504c2f883ab5d5c0a
Bogdan Zhurakovskyi <bogdan.zhurakovskyi@cloudfactory.com>
2024-08-07 16:41:11 +0200

    Change requirements

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     packages=find_packages(include=("romatch*",)),
     version="0.0.1",
     author="Johan Edstedt",
-    install_requires=open("requirements.txt", "r").read().split("\n"),
+    install_requires=open("requirements_inf.txt", "r").read().split("\n"),
 )
