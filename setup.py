7906fb2eae45a88bed2137df90f4e3659a76e6fc
Johan Edstedt <johan.edstedt@liu.se>
2025-02-16 02:54:59 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="romatch",
     packages=find_packages(include=("romatch*",)),
-    version="0.0.1",
+    version="0.0.2",
     author="Johan Edstedt",
     install_requires=open("requirements.txt", "r").read().split("\n"),
 )
